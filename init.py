c09eb2996353f8e634bd0100d707f41f105c8fa9
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2023-11-13 16:11:37 +0900

    initialize_instanceを使うことにした

--- init.py
+++ init.py
@@ -3,9 +3,6 @@ import R2D2
 caseid = R2D2.util.caseid_select(locals())
 datadir="../run/"+caseid+"/data/"
 
-try:
-    d
-except NameError:
-    d = None
+R2D2.util.initialize_instance(locals(),'d')    
 d = R2D2.R2D2_data(datadir,verbose=True,self_old=d)
 R2D2.util.locals_define(d,locals())
