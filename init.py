5cea41831d4fa07f69b19e5fecc29ee4c0a65c82
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2023-11-13 15:36:49 +0900

    前のコミットのaddし忘れ

--- init.py
+++ init.py
@@ -3,5 +3,9 @@ import R2D2
 caseid = R2D2.util.caseid_select(locals())
 datadir="../run/"+caseid+"/data/"
 
-d = R2D2.R2D2_data(datadir,verbose=True)
+try:
+    d
+except NameError:
+    d = None
+d = R2D2.R2D2_data(datadir,verbose=True,self_old=d)
 R2D2.util.locals_define(d,locals())
