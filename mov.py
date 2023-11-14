465fd380d12903cc6b9e1eb65bc9b4792d5ae43a
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2023-11-14 20:25:46 +0900

    調整

--- mov.py
+++ mov.py
@@ -6,28 +6,17 @@ import R2D2
 import sys
 import os
 
-try:
-    caseid
-except NameError:
-    print("input caseid id (3 digit)")
-    caseid = 0
-    caseid = input()
-    caseid = "d"+caseid.zfill(3)
-    
+caseid = R2D2.util.caseid_select(locals())
 datadir="../run/"+caseid+"/data/"
+
+R2D2.util.initialize_instance(locals(),'d')
+d = R2D2.R2D2_data(datadir,self_old=d)
+R2D2.util.locals_define(d,locals())
+
 pngdir="../figs/"+caseid+"/mov/"
 os.makedirs(pngdir,exist_ok=True)
 
-d = R2D2.R2D2_data(datadir)
-for key in d.p:
-    exec('%s = %s%s%s' % (key, 'd.p["',key,'"]'))
-    
-try:
-    n0
-except NameError:
-    n0 = 0
-if  n0 > d.p["nd"]:
-    n0 = d.p["nd"]
+R2D2.util.define_n0(d,locals())
 
 print("Maximum time step= ",nd," time ="\
           ,dtout*float(nd)/3600./24.," [day]")
@@ -46,11 +35,7 @@ fig = plt.figure(num=1,figsize=(xsize,ysize))
 
 grid = GridSpec(2,2,height_ratios=[yran,xran])
 
-te2, tmp = np.meshgrid(te0,y,indexing='ij')
-
 for n in tqdm(range(n0,nd+1)):
-#for n in range(0,1):
-    #print(n)
     ##############################
     # read time
     t = d.read_time(n,silent=True)
@@ -62,8 +47,8 @@ for n in tqdm(range(n0,nd+1)):
 
     ##############################
     # read value
-
     d.read_vc(n,silent=True)
+    
     ##############################
 
     shading = "auto"
@@ -75,49 +60,38 @@ for n in tqdm(range(n0,nd+1)):
     ax3 = fig.add_subplot(grid[1,0],aspect='equal')
     ax4 = fig.add_subplot(grid[1,1],aspect='equal')
     
-    ax1.tick_params(labelbottom=False)
-    if deep_flag == 1:
-        d.read_qq_select(xmax,n,silent=True)
-        in0 = d.qs["vx"].copy()
-        in0s = np.roll(in0,[jx//2-jc,kx//2-kc],axis=[0,1])
-        ax1.pcolormesh(y*lfac,z*lfac,in0s.transpose(),cmap='gist_gray',vmax=1.e4,vmin=-1.e4,shading=shading)
-        ax1.set_ylabel("z [Mm]")
-        ax1.set_title("Vertical velocity")
-        sem, tmp = np.meshgrid(d.vc['sem'].mean(axis=1),y,indexing='ij')
-        ax3.pcolormesh(y*lfac,(x-rstar)*lfac,(d.vc['se_xy']-d.vc['sem'])/d.vc['serms'],cmap='gist_heat',vmax=3,vmin=-3)
-        ax3.set_title(r'$(s-\langle s\rangle)/s_{rms}$')
-    else:
-        in0 = d.qt["in"].copy()
-        inm = in0.mean()
-        inrms = np.sqrt(((in0 - in0.mean())**2).mean())
-        in0s = np.roll(in0,[jx//2-jc,kx//2-kc],axis=[0,1])
-        frms = 2.
-        ax1.pcolormesh(y*lfac,z*lfac,in0s.transpose(),cmap='inferno',vmax=inm+inrms*frms,vmin=inm-inrms*frms,shading=shading)
-        ax1.set_ylabel("z [Mm]")
-        ax1.set_title("Emergent intensity")
-        ax3.pcolormesh(y*lfac,(x-rstar)*lfac,(d.vc['se_xy'] - d.vc['sem'])/d.vc['serms'],vmin=-2.,vmax=2.,shading=shading)
-        tus = np.roll(d.vc["tu_xy"],[jx//2-jc],axis=1)
-        ax3.contour(y*lfac,(x-rstar)*lfac,tus,levels=[1.],colors="w")
-        ax3.set_title(r"$T$")
-        ax4.contour(y*lfac,(x-rstar)*lfac,tus,levels=[1.],colors="w")
-
-    bx = np.roll(d.qt["bx"],[jx//2-jc,kx//2-kc],axis=[0,1])
-    ax2.tick_params(labelbottom=False)
-    ax2.tick_params(labelleft=False)
-    ax2.pcolormesh(y*lfac,z*lfac,bx.transpose(),cmap='gist_gray',vmax=2.5e3,vmin=-2.5e3,shading=shading)
-    ax2.set_title(r"LOS magnetic field@$\tau=1$")
-
-    ax3.set_ylim((max(xmax-yran,xmin)-rstar)*lfac,(xmax-rstar)*lfac)
-    ax3.set_ylabel("$x$ [Mm]")
-    ax3.set_xlabel("$y$ [Mm]")
+    inm = d.qt['in'].mean()
+    inrms = np.sqrt(((d.qt['in'] - inm)**2).mean())
+    frms = 2.
+    
+    # pcolormesh    
+    ax1.pcolormesh(y*lfac,z*lfac,d.qt['in'].T,cmap='inferno',vmax=inm+inrms*frms,vmin=inm-inrms*frms,shading=shading)
+    ax2.pcolormesh(y*lfac,z*lfac,d.qt['bx'].T,cmap='gist_gray',vmax=2.5e3,vmin=-2.5e3,shading=shading)
+    ax3.pcolormesh(y*lfac,(x-rstar)*lfac,(d.vc['se_xy'] - d.vc['sem'])/d.vc['serms'],vmin=-2.,vmax=2.,shading=shading)
+    ax3.contour(y*lfac,(x-rstar)*lfac,d.vc['tu_xy'],levels=[1.],colors="w")
     
     bb = np.sqrt(d.vc["bx_xy"]**2 + d.vc["by_xy"]**2 + d.vc["bz_xy"]**2)
-    bbs = np.roll(bb,[jx//2-jc],axis=1)
-    ax4.tick_params(labelleft=False)
-    ax4.pcolormesh(y*lfac,(x-rstar)*lfac,bbs,vmax=8.e3,vmin=0.,cmap='gist_heat',shading=shading)
-    ax4.set_ylim((max(xmax-yran,xmin)-rstar)*lfac,(xmax-rstar)*lfac)
-    ax4.set_xlabel("$y$ [Mm]")
+    ax4.pcolormesh(y*lfac,(x-rstar)*lfac,bb,vmax=8.e3,vmin=0.,cmap='gist_heat',shading=shading)
+    
+    ax4.contour(y*lfac,(x-rstar)*lfac,d.vc['tu_xy'],levels=[1.],colors="w")
+
+    for ax in [ax1,ax2]:
+        ax.tick_params(labelbottom=False)
+    for ax in [ax2,ax4]:
+        ax.tick_params(labelleft=False)
+    for ax in [ax3,ax4]:
+        ax.set_xlabel('$y$ [Mm]')
+    for ax in [ax1,ax3]:
+        ax.set_ylabel('$x$ [Mm]')
+    
+    for ax in [ax3,ax4]:
+        ax.set_ylim((max(xmax-yran,xmin)-rstar)*lfac,(xmax-rstar)*lfac)
+        
+    ax1.set_title('Emergent intensity')        
+    ax2.set_title(r"LOS magnetic field at $\tau=1$")
+    ax3.set_title(r"$(s-\langle s\rangle)/s_\mathrm{RMS}$")
     ax4.set_title(r"$|B|$")
+    
 
     ax3.annotate(text="$t$="+"{:.2f}".format((t-t0)/60/60)+" [hour]"\
                      ,xy=[0.03,0.03],xycoords="figure fraction"\
