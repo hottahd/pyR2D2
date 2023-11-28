29172a61ef22375df613a697cccbdc692f608e37
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2023-11-28 14:26:49 +0900

    movとmov_sphericalを一つにまとめた

--- mov.py
+++ mov.py
@@ -1,10 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
 from tqdm import tqdm
 import R2D2
-import sys
-import os
+import sys, os
+import matplotlib.pyplot as plt
+import mov_util
 
 caseid = R2D2.util.caseid_select(locals())
 datadir="../run/"+caseid+"/data/"
@@ -23,108 +22,113 @@ print("Maximum time step= ",nd," time ="\
 
 plt.close('all')
 
+if not d.p['geometry'] == 'Cartesian':
+    d.p['zz'],d.p['yy'] = np.meshgrid(d.p['z'],d.p['y']-0.5*np.pi)
+    
+    xe = np.zeros(d.p['ix']+1)
+    ye = np.zeros(d.p['jx']+1)
+
+    xe[0] = d.p['xmin']
+    xe[d.p['ix']] = d.p['xmax']
+    ye[0] = 0.e0
+    ye[d.p['jx']] = np.pi
+    for i in range(1,d.p['ix']):
+        xe[i] = xe[i-1] + 2*(d.p['x'][i-1] - xe[i-1])
+
+    for j in range(1,d.p['jx']):
+        ye[j] = ye[j-1] + 2*(d.p['y'][j-1] - ye[j-1])
+
+    RAE, THE = np.meshgrid(xe,ye,indexing='ij')
+    d.p['XX'], d.p['YY'] = RAE*np.cos(THE), RAE*np.sin(THE)
+
 # read initial time
 t0 = d.read_time(0,silent=True)
-
-zran = zmax - zmin
-yran = ymax - ymin
-xran = min(xmax-xmin,zran)
-
-xsize = 18
-ysize = xsize*(yran + xran)/2/zran
-ysize = ysize + min(xsize/ysize/1.5,2)
-ysize_limit = 11
-if ysize > ysize_limit:
-    xsize = xsize/ysize*ysize_limit
-    ysize = ysize_limit
-fig = plt.figure(num=1,figsize=(xsize,ysize))
-
-grid = GridSpec(2,2,height_ratios=[yran,xran])
 
 for n in tqdm(range(n0,nd+1)):
     # read data
     t = d.read_time(n,silent=True) 
-    d.read_qq_tau(n*int(ifac),silent=True)
     d.read_vc(n,silent=True)
     
     ##############################
+    
+    if n == n0:
+        tight_layout_flag = True
+    else:
+        tight_layout_flag = False
+    
+    if d.p['geometry'] == 'Cartesian':
+        d.read_qq_tau(n*int(d.p['ifac']),silent=True)        
+        inm = d.qt['in'].mean() # mean intensity
+        inrms = np.sqrt(((d.qt['in'] - inm)**2).mean()) # RMS intensity
+        frms = 2.
 
-    shading = "auto"
-
-    lfac = 1.e-8
-    
-    ax1 = fig.add_subplot(grid[0,0],aspect='equal')
-    ax2 = fig.add_subplot(grid[0,1],aspect='equal')
-    ax3 = fig.add_subplot(grid[1,0],aspect='equal')
-    ax4 = fig.add_subplot(grid[1,1],aspect='equal')
-    
-    inm = d.qt['in'].mean()
-    inrms = np.sqrt(((d.qt['in'] - inm)**2).mean())
-    frms = 2.
-    
-    # pcolormesh    
-    im1 = ax1.pcolormesh(z*lfac,y*lfac,d.qt['in']*1.e-10,cmap='inferno',vmax=(inm+inrms*frms)*1.e-10,vmin=(inm-inrms*frms)*1.e-10,shading=shading)
-    im2 = ax2.pcolormesh(z*lfac,y*lfac,d.qt['bx'],cmap='gist_gray',vmax=2.5e3,vmin=-2.5e3,shading=shading)
-    sem, tmp = np.meshgrid(d.vc['sem'].mean(axis=1),z,indexing='ij')
-    serms, tmp = np.meshgrid(np.sqrt((d.vc['serms']**2).mean(axis=1)),z,indexing='ij')
-    
-    im3 = ax3.pcolormesh(z*lfac,(x-rstar)*lfac,(d.vc['se_xz'] - sem)/serms,vmin=-2.,vmax=2.,shading=shading)
-    ax3.contour(z*lfac,(x-rstar)*lfac,d.vc['tu_xz'],levels=[1.],colors="w")
-    
-    bb = np.sqrt(d.vc["bx_xz"]**2 + d.vc["by_xz"]**2 + d.vc["bz_xz"]**2)
-    im4 = ax4.pcolormesh(z*lfac,(x-rstar)*lfac,bb,vmax=8.e3,vmin=0.,cmap='gist_heat',shading=shading)
-    
-    ax4.contour(z*lfac,(x-rstar)*lfac,d.vc['tu_xz'],levels=[1.],colors="w")
-
-    for ax in [ax1,ax2]:
-        ax.tick_params(labelbottom=False)
-    for ax in [ax2,ax4]:
-        ax.tick_params(labelleft=False)
-    for ax in [ax3,ax4]:
-        ax.set_xlabel('$z$ [Mm]')
-    ax1.set_ylabel('$y$ [Mm]')
-    ax3.set_ylabel('$x$ [Mm]')
-    
-    for ax in [ax3,ax4]:
-        ax.set_ylim((max(xmax-yran,xmin)-rstar)*lfac,(xmax-rstar)*lfac)
-    
-    titles = ['Emergent intensity\n'+r' $\mathrm{[10^{10}~erg~cm^{-2}~ster^{-1}~s^{-1}]}$',
-              "LOS magnetic field\n"+r"at $\tau=1~\mathrm{[G]}$",
-              r"$\left(s-\langle s\rangle\right)/s_\mathrm{RMS}$",
-              r"$|B|~\mathrm{[G]}$"
-    ]
-    for ax, title in zip([ax1,ax2,ax3,ax4],titles):
-        title = ax.set_title(title)
-        title.set_position([0.01,1.02])
-        title.set_ha('left')
+        sem, tmp = np.meshgrid(d.vc['sem'].mean(axis=1),z,indexing='ij')
+        serms, tmp = np.meshgrid(np.sqrt((d.vc['serms']**2).mean(axis=1)),z,indexing='ij')
         
-    if(n == n0):
-        fig.tight_layout()
+        bb = np.sqrt(d.vc["bx_xz"]**2 + d.vc["by_xz"]**2 + d.vc["bz_xz"]**2)        
+
+        vls = [d.qt['in']*1.e-10,
+               d.qt['bx'],
+               (d.vc['se_xz'] - sem)/serms,
+               bb
+               ]
+        vmaxs = [(inm+inrms*frms)*1.e-10, # intensity
+                 2.5e3, # LoS B
+                 2., # entropy
+                 8.e3, # magnetic field strength
+                 ]
+        vmins = [(inm-inrms*frms)*1.e-10, # intensity
+                 -2.5e3, # LoS B
+                 -2., # entropy
+                 0 # magnetic field strength
+                 ]
+        titles = ['Emergent intensity\n'+r' $\mathrm{[10^{10}~erg~cm^{-2}~ster^{-1}~s^{-1}]}$',
+                  "LOS magnetic field\n"+r"at $\tau=1~\mathrm{[G]}$",
+                  r"$\left(s-\langle s\rangle\right)/s_\mathrm{RMS}$",
+                  r"$|B|~\mathrm{[G]}$"
+                ]
+        mov_util.mov_cartesian_2x2(d,t,vls,vmaxs,vmins,titles,tight_layout_flag)
+    else:
+        d.read_qq_select(xmax,n,silent=True)
+        vxrms = np.sqrt((d.qs['vx']**2).mean())
+        bxrms = np.sqrt((bx**2).mean())
         
-    ax3.annotate(text="$t="+"{:.2f}".format((t-t0)/60/60)+"~\mathrm{[hour]}$"\
-                     ,xy=[0.01,0.02],xycoords="figure fraction"\
-                     ,color='black',fontsize=20)
-    
-    # add color bar
-    box3 = ax3.get_position().bounds
-    wid3, hei3 = box3[2], box3[3]
-    
-    color_bar_width = 0.12
-    color_bar_height = 0.08/ysize
-    
-    for ax, im in zip([ax1,ax2,ax3,ax4],[im1,im2,im3,im4]):
-        box = ax.get_position().bounds
-        cax = fig.add_axes([box[0]+ box[2] - color_bar_width
-                        ,box[1] + box[3] + color_bar_height*4
-                        ,color_bar_width
-                        ,color_bar_height
-                        ])
-        cbar = fig.colorbar(im,cax=cax,orientation='horizontal')
-        cbar.ax.tick_params(labelsize=12)
-    
-    
+        sem, tmp   = np.meshgrid((d.vc['sem']*SINY).sum(axis=1)/SINYM,d.p['y'],indexing='ij')
+        serms, tmp = np.meshgrid(np.sqrt((d.vc['serms']**2*SINY).sum(axis=1)/SINYM),d.p['y'],indexing='ij')
+        
+        if serms.max() != 0:
+            se_value = (d.vc['se_xy']-sem)/serms
+        else:
+            se_value = np.zeros((ix,jx))
+        
+        vls = [d.qs['vx']*1.e-2,
+               d.qs['bx']*1.e-3,
+               se_value,
+               d.vc['bzm']
+               ]
+        vmaxs = [2*vxrms*1.e-2, # radial velocity
+                 2*bxrms*1.e-3, # radial magnetic field
+                 2., # entropy
+                 8000, # magnetic field strength
+                 ]
+        vmins = [-vmaxs[0], # radial velocity
+                 -vmaxs[1], # radial magnetic field
+                 -2., # entropy
+                 -8000 # magnetic field strength
+                 ]
+        titles = [r'Radial velocity $v_r~[\mathrm{m~s^{-1}}]$',
+                  r'Radial magnetic field $B_r~[\mathrm{kG}]$',
+                  r"$\left(s-\langle s\rangle\right)/s_\mathrm{RMS}$",
+                  r"$\langle B_\phi\rangle~\mathrm{[G]}$"
+                ]
+                
+        mov_util.mov_spherical_2x2(d,t,vls,vmaxs,vmins,titles,tight_layout_flag)
     plt.pause(0.1)
     plt.savefig(pngdir+"py"+'{0:08d}'.format(n)+".png")
-
     if(n != nd):
         plt.clf()
+        
+
+        
+    
+ 
---
+++ mov_util.py
@@ -0,0 +1,129 @@
+def add_color_bar_2x2(fig,axes,ims,ysize):
+    # add color bar
+    box3 = axes[2].get_position().bounds
+    wid3, hei3 = box3[2], box3[3]
+    
+    color_bar_width = 0.12
+    color_bar_height = 0.08/ysize
+    
+    for ax, im in zip(axes,ims):
+        box = ax.get_position().bounds
+        cax = fig.add_axes([box[0]+ box[2] - color_bar_width
+                        ,box[1] + box[3] + color_bar_height*4
+                        ,color_bar_width
+                        ,color_bar_height
+                        ])
+        cbar = fig.colorbar(im,cax=cax,orientation='horizontal')
+        cbar.ax.tick_params(labelsize=12)
+
+def mov_cartesian_2x2(d,t,vls,vmaxs,vmins,titles,tight_layout_flag=True):
+    import matplotlib.pyplot as plt
+    from matplotlib.gridspec import GridSpec
+
+    zran = d.p['zmax'] - d.p['zmin']
+    yran = d.p['ymax'] - d.p['ymin']
+    xran = min(d.p['xmax']-d.p['xmin'],zran)
+
+    xsize = 18
+    ysize = xsize*(yran + xran)/2/zran
+    ysize = ysize + min(xsize/ysize/1.5,2)
+    ysize_limit = 11
+    if ysize > ysize_limit:
+        xsize = xsize/ysize*ysize_limit
+        ysize = ysize_limit
+    fig = plt.figure(num=1,figsize=(xsize,ysize))
+
+    grid = GridSpec(2,2,height_ratios=[yran,xran])    
+    
+    ax1 = fig.add_subplot(grid[0,0],aspect='equal')
+    ax2 = fig.add_subplot(grid[0,1],aspect='equal')
+    ax3 = fig.add_subplot(grid[1,0],aspect='equal')
+    ax4 = fig.add_subplot(grid[1,1],aspect='equal')
+        
+    shading = "auto"
+    lfac = 1.e-8 # unit is Mm
+    
+    ims = []
+    axes = [ax1,ax2,ax3,ax4]
+    # pcolormesh    
+    for ax, vl, vmax, vmin in zip(axes[:2],vls[:2],vmaxs[:2],vmins[:2]):
+        ims.append(ax.pcolormesh(d.p['z']*lfac,d.p['y']*lfac,vl,cmap='inferno',vmax=vmax,vmin=vmin,shading=shading))
+    
+    for ax, vl, vmax, vmin in zip(axes[2:],vls[2:],vmaxs[2:],vmins[2:]):
+        ims.append(ax.pcolormesh(d.p['z']*lfac,(d.p['x']-d.p['rstar'])*lfac,vl,vmin=vmin,vmax=vmax,shading=shading))
+        ax.contour(d.p['z']*lfac,(d.p['x']-d.p['rstar'])*lfac,d.vc['tu_xz'],levels=[1.],colors="w")
+    
+    for ax in [ax1,ax2]:
+        ax.tick_params(labelbottom=False)
+    for ax in [ax2,ax4]:
+        ax.tick_params(labelleft=False)
+    for ax in [ax3,ax4]:
+        ax.set_xlabel('$z$ [Mm]')
+    ax1.set_ylabel('$y$ [Mm]')
+    ax3.set_ylabel('$x$ [Mm]')
+    
+    for ax in [ax3,ax4]:
+        ax.set_ylim((max(d.p['xmax']-yran,d.p['xmin'])-d.p['rstar'])*lfac,(d.p['xmax']-d.p['rstar'])*lfac)
+    
+    for ax, title in zip([ax1,ax2,ax3,ax4],titles):
+        title = ax.set_title(title)
+        title.set_position([0.01,1.02])
+        title.set_ha('left')
+        
+    if tight_layout_flag:
+        fig.tight_layout()
+        
+    ax3.annotate(text="$t="+"{:.2f}".format((t)/60/60)+"~\mathrm{[hour]}$"\
+                     ,xy=[0.01,0.02],xycoords="figure fraction"\
+                     ,color='black',fontsize=20)
+    
+    # add color bar
+    add_color_bar_2x2(fig,axes,ims,ysize)
+
+def mov_spherical_2x2(d,t,vls,vmaxs,vmins,titles,tight_layout_flag=True):
+    import matplotlib.pyplot as plt
+    from matplotlib.gridspec import GridSpec
+    
+    xsize = 16
+    ysize = 9
+    fig = plt.figure(num=1,figsize=(xsize,ysize))
+        
+    ax1 = fig.add_subplot(221,projection='mollweide')
+    ax2 = fig.add_subplot(222,projection='mollweide')
+    ax3 = fig.add_subplot(223,aspect='equal')
+    ax4 = fig.add_subplot(224,aspect='equal')
+    
+    shading = "auto"    
+    ims = []
+    axes = [ax1,ax2,ax3,ax4]
+    
+    lfac = 1/d.p['rstar']
+    
+    for ax, title in zip(axes,titles):
+        title = ax.set_title(title)
+        title.set_position([0.01,1.02])
+        title.set_ha('left')
+        
+                
+    for ax, vl, vmax, vmin in zip(axes[:2],vls[:2],vmaxs[:2],vmins[:2]):
+        ims.append(ax.pcolormesh(d.p['zz'],d.p['yy'],vl,shading=shading,cmap='inferno',vmax=vmax,vmin=vmin))
+        ax.set_xticklabels('')
+        ax.set_yticklabels('')
+    
+    for ax, vl, vmax, vmin in zip(axes[2:],vls[2:],vmaxs[2:],vmins[2:]):
+        ims.append(ax.pcolormesh(d.p['XX'].T*lfac,d.p['YY'].T*lfac,vl.T,shading=shading,cmap='inferno',vmax=vmax,vmin=vmin))
+        ax.set_xlabel(r'$z/R_*$')
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        
+    ax3.set_ylabel(r'$x/R_*$')
+        
+    if tight_layout_flag:
+        fig.tight_layout()
+        
+    ax3.annotate(text="$t="+"{:.2f}".format((t)/60/60/24)+"~\mathrm{[day]}$"\
+                    ,xy=[0.01,0.02],xycoords="figure fraction"\
+                    ,color='black',fontsize=20)
+    
+    # add color bar
+    add_color_bar_2x2(fig,axes,ims,ysize)
