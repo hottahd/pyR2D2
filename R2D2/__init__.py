279cfa00ca357b5759024ff19310c6b8447b43ca
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2024-11-30 13:56:52 +0900

    .gitignoreの更新とR2D2モジュールのインポートパスを修正

--- R2D2/__init__.py
+++ R2D2/__init__.py
@@ -1,6 +1,6 @@
-from R2D2.common import *
-from R2D2.fortran_util import *
-from R2D2.vtk import *
+from .R2D2_data import *
+from .fortran_util import *
+from .vtk import *
 from . import util
 
 #from . import google
