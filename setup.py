3579e37bc5ec54c81c487e080fd89e4fb2de5ca7
Hideyuki Hotta <hotta.hideyuki@gmail.com>
2024-12-18 16:54:51 +0900

    setup.pyの更新：対象パッケージを指定するためにfind_packagesを追加

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         "relative_to": __file__,
     },
     setup_requires=["setuptools_scm"],
+    packages=find_packages(include=["pyR2D2", "pyR2D2.*"]),  # 対象パッケージを指定
     include_package_data=True,
     cmdclass={"build_py": CustomBuildPy},  # build_pyをカスタムコマンドに置き換え
     zip_safe=False,
